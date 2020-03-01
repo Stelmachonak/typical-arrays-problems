7e254588913f1c965eca9a2a33992216d6ae186a
Stelmachonak <362060@mail.ru>
2020-03-01 13:28:15 +0300

    feat: add solution for task tyical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let min = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let avg = 0;
+    for (let i = 0; i < array.length; i++) {
+        avg += array[i];
+    }
+    return avg / array.length;
+};
